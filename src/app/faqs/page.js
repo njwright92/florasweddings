7456bf5fe8cab271fdf0a915137617623d8e97bb
njwright92 <njwright92@gmail.com>
2023-12-28 14:04:02 -0800

    "starting app pages"

---
+++ src/app/faqs/page.js
@@ -0,0 +1,7 @@
+export default function Faqs() {
+    return (
+        <div>
+            <h1>FAQs</h1>
+        </div>
+    );
+}
---
+++ src/app/gallery/page.js
@@ -0,0 +1,7 @@
+export default function Gallery() {
+    return (
+        <div>
+            <h1>Gallery</h1>
+        </div>
+    );
+}
